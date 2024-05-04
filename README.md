81432e406175a174e5a323b8746ffcd67286a12c
Drew Immerman <drewimmerman@gmail.com>
2024-05-04 10:12:50 -0400

    Project created using TypeScript Template (lib)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# {{project-name}} - Readme
+# riao-server - Readme
 
 ## Contributing & Development
 
--- test/spec/index.spec.ts
+++ test/spec/index.spec.ts
@@ -1,7 +1,7 @@
 import 'jasmine';
 import * as index from '../../src';
 
-describe('{{project-name}}', () => {
+describe('riao-server', () => {
 	it('exports a', () => {
 		expect(index.a).toBeTrue();
 	});
