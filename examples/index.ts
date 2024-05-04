3747e071248b0457066ad27baee65fda4a0e775c
Drew Immerman <drewimmerman@gmail.com>
2024-05-04 10:21:16 -0400

    Move riao node project to library example folder

---
+++ examples/index.ts
@@ -0,0 +1,10 @@
+/**
+ * This file is the main entrypoint for your application.
+ */
+import { bootstrap } from './app';
+import { main } from './main';
+
+/**
+ * Bootstrap the application
+ */
+bootstrap(main);
