816a7ed5dc53c01d2bef1fbf51ff496282999dc9
Drew Immerman <drewimmerman@gmail.com>
2024-05-05 10:01:53 -0400

    Add server unit test

---
+++ test/spec/server.spec.ts
@@ -0,0 +1,53 @@
+import 'jasmine';
+
+import { RiaoHttpClient } from '@riao/http-client';
+
+import { ActionRequest, ControllerInterface, RiaoServer } from '../../src';
+import { env } from '../../examples/env';
+
+class TestBasicController implements ControllerInterface {
+	path = 'test';
+	name = 'Test Controller';
+
+	actions = {
+		echo: async (request: ActionRequest) => {
+			return {
+				hello: 'world',
+				echo: request.body.echo,
+			};
+		},
+	};
+}
+
+class TestServer extends RiaoServer {
+	port = env.HTTP_PORT;
+	controllers = [TestBasicController];
+}
+
+describe('Server', () => {
+	let server: RiaoServer;
+	let client: RiaoHttpClient;
+
+	beforeAll(async () => {
+		server = new TestServer();
+		server.start();
+
+		client = new RiaoHttpClient();
+		client.url = `http://localhost:${env.HTTP_PORT}/api/v1`;
+	});
+
+	it('is running', async () => {
+		const response = await client.request({
+			path: '/test/echo',
+			method: 'POST',
+			body: {
+				echo: 'teststring',
+			},
+		});
+
+		expect(response).toEqual({
+			hello: 'world',
+			echo: 'teststring',
+		});
+	});
+});
