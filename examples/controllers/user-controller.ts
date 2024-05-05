8b9b2b5ac70f373345ea8ec9558c379c5817880d
Drew Immerman <drewimmerman@gmail.com>
2024-05-05 08:57:45 -0400

    Fix UserController login action

--- examples/controllers/user-controller.ts
+++ examples/controllers/user-controller.ts
@@ -2,14 +2,15 @@ import { ModelController } from '../../src/controller';
 
 import { maindb } from '../../database/main';
 import { User } from '../../examples/models/user';
+import { ActionRequest } from 'src';
 
 export class UserController extends ModelController<User> {
 	path = 'users';
 	repo = maindb.getQueryRepository<User>({ table: 'users' });
 
 	actions = {
-		login: async (request: Partial<User>) => {
-			console.log('User is trying to login!', request.username);
+		login: async (request: ActionRequest) => {
+			console.log('User is trying to login!', request.body.username);
 		},
 	};
 }
