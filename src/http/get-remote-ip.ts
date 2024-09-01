663d11d4faf6291163f5be9f927fd37447743b86
Drew Immerman <drewimmerman@gmail.com>
2024-09-01 07:03:42 -0400

    Fix getRemoteIP for proxies

--- src/http/get-remote-ip.ts
+++ src/http/get-remote-ip.ts
@@ -1,5 +1,27 @@
 import { Request as ExpressRequest } from 'express';
 
 export function getRemoteIp(request: ExpressRequest): string {
-	return request.headers['x-forwarded-for'] || request.socket.remoteAddress;
+	let forwardIps: string[] = [];
+	const forwardedHeader: undefined | string | string[] =
+		request.headers['x-forwarded-for'];
+
+	if (forwardedHeader) {
+		forwardIps = Array.isArray(forwardedHeader)
+			? forwardedHeader
+			: [forwardedHeader];
+	}
+
+	let ips: string[] = [];
+
+	const remoteIp: undefined | string = request.socket.remoteAddress;
+
+	if (remoteIp) {
+		ips.push(remoteIp);
+	}
+
+	if (forwardIps.length) {
+		ips = ips.concat(forwardIps);
+	}
+
+	return ips.join(', ');
 }
